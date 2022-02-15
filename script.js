08cd6087dc9156db0df234050d64a1cf95c8e95a
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-15 13:40:14 -0600

    conditions written but not working right

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function generatePassword() {
     // Password Length: at least 8 no more than 128
     let passwordLength = prompt(`Enter a password length between 8 and 128`);
     console.log(`User selected: password length ${passwordLength}`);
-    
+
     // check to make sure user selects a valid password length
     if (passwordLength < 7 || passwordLength > 129) {
         alert(`Password must be between 8 and 128 characters`);
@@ -30,12 +30,6 @@ function generatePassword() {
     // Special characters
     var includeSpecial = confirm(`Include special characters?`);
     console.log(`User selected: special ${includeSpecial}`);
-    // Check that user selected at least one valid character type
-    if (!includeUpper && !includeLower && !includeNumeric && !includeSpecial) {
-        alert(`User must select at least one character type.`);
-        console.log(`User must select at least one character type.`);
-        return;
-    }
 
     // Make arrays for the possible character types
     const alphaUpper = [
@@ -92,8 +86,8 @@ function generatePassword() {
         "w",
         "x",
         "y",
-        "z"
-    ];  
+        "z",
+    ];
     const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     const charSpecial = [
         " ",
@@ -133,28 +127,115 @@ function generatePassword() {
 
     var characters = new Array();
 
-     // Generate character array based on options user selected, starting with edge cases
-     if (includeUpper && includeLower && includeNumeric && includeSpecial) {
-         characters = alphaUpper.concat(alphaLower, numbers, charSpecial);
-         console.log(characters);
-     }
-     else if (includeUpper && includeLower && includeNumeric && !includeSpecial) {
+    // Generate character array based on options user selected, starting with edge cases
+    // User selects all options
+    if (includeUpper && includeLower && includeNumeric && includeSpecial) {
+        characters = alphaUpper.concat(alphaLower, numbers, charSpecial);
+        console.log(characters);
+    }
+    // Upper inclusive options
+    else if (
+        includeUpper &&
+        includeLower &&
+        includeNumeric &&
+        !includeSpecial
+    ) {
         characters = alphaUpper.concat(alphaLower, numbers);
         console.log(characters);
-     }
+    } else if (
+        includeUpper &&
+        includeLower &&
+        !includeNumeric &&
+        !includeSpecial
+    ) {
+        characters = alphaUpper.concat(alphaLower);
+        console.log(characters);
+    } else if (
+        includeUpper &&
+        !includeLower &&
+        !includeNumeric &&
+        !includeSpecial
+    ) {
+        for (i = 0; i < alphaUpper.length; i++) {
+            characters[i] = alphaUpper[i];
+        }
+        console.log(characters);
+    }
+    // Lower inclusive options, no upper
+    else if (
+        includeLower &&
+        includeNumeric &&
+        includeSpecial &&
+        !includeUpper
+    ) {
+        characters = alphaLower.concat(numbers, charSpecial);
+        console.log(characters);
+    } else if (
+        includeLower &&
+        includeNumeric &&
+        !includeSpecial &&
+        !includeUpper
+    ) {
+        characters = alphaLower.concat(numbers);
+        console.log(characters);
+    } else if (
+        includeLower &&
+        !includeNumeric &&
+        includeSpecial &&
+        !includeUpper
+    ) {
+        for (i = 0; i < alphaLower.length; i++) {
+            characters[i] = alphaLower[i];
+        }
+        console.log(characters);
+    }
+    // Numeric inclusive options, no alphabet
+    else if (
+        includeNumeric &&
+        includeSpecial &&
+        !includeUpper &&
+        !includeLower
+    ) {
+        characters = numbers.concat(includeSpecial);
+        console.log(characters);
+    } else if (
+        includeNumeric &&
+        !includeSpecial &&
+        !includeUpper &&
+        !includeLower
+    ) {
+        for (i = 0; i < numbers.length; i++) {
+            characters[i] = numbers[i];
+        }
+        console.log(characters);
+    } else if (
+        includeSpecial &&
+        !includeUpper &&
+        !includeLower &&
+        !includeNumeric
+    ) {
+        for (i = 0; i < charSpecial.length; i++) {
+            characters[i] = charSpecial[i];
+        }
+    }
+    // Check that user selected at least one valid character type
+    else {
+        alert(`User must select at least one character type.`);
+        console.log(`User must select at least one character type.`);
+        return;
+    }
 
-     // Populate password array with random characters
-          // Make an array to put the password in
+    // Populate password array with random characters
+    // Make an array to put the password in
     var password = new Array(passwordLength);
-  
-     // Randum number generation for each spot in the password
-     for (i = 0; i < passwordLength; i++) {
-        password[i] = characters[Math.floor(Math.random() * characters.length)]
-     }
-     console.log(password);
 
+    // Randum number generation for each spot in the password
+    for (i = 0; i < passwordLength; i++) {
+        password[i] = characters[Math.floor(Math.random() * characters.length)];
+    }
+    console.log(password);
 
-return password.join("");
+    return password.join("");
 }
 
 // Write password to the #password input
