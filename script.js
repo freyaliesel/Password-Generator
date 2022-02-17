ed9b64811b5ef8369b174d6c2d1d5ecfde48c38c
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-02-16 18:58:37 -0600

    formatting adjustments

--- script.js
+++ script.js
@@ -23,12 +23,14 @@ function generatePassword() {
     }
 
     // Get desired password length
-    var passwordLength = parseInt(document.getElementById("passwordLength").value);
-    
+    var passwordLength = parseInt(
+        document.getElementById("passwordLength").value
+    );
+
     // Make sure it's a number
-    if (isNaN(passwordLength)){
-        console.log('User entered a NaN')
-        return "Password must be a number."
+    if (isNaN(passwordLength)) {
+        console.log("User entered a NaN");
+        return "Password must be a number.";
     }
     console.log(`User entered: ${passwordLength} as password length.`);
 
